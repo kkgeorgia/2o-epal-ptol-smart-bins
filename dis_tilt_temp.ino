1940083d63f5ff9211c6216a9e213cb91ac13c46
Katerina Georgiadou <kkgeorgia@sch.gr>
2025-05-26 00:36:50 +0300

    Add files via upload

---
+++ dis_tilt_temp.ino
@@ -0,0 +1,93 @@
+#include <WiFiS3.h>
+#include <ThingSpeak.h>
+
+char ssid[] = "CYTA1BBF";       
+char pass[] = "ZTETG8FGB303788";
+
+WiFiClient client;
+
+unsigned long channelID = 2902543;  
+const char* writeAPIKey = "6W5Q7JYJDM7YT2PO";
+
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define TILT_PIN 2  
+#define DHTPIN 5     
+#define DHTTYPE DHT22  
+#include <DHT.h> 
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+    Serial.begin(9600);
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    pinMode(TILT_PIN, INPUT);
+    Serial.println("DHTxx test!");
+    dht.begin();
+
+    WiFi.begin(ssid, pass);
+    Serial.print("Σύνδεση στο WiFi...");
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nΣυνδέθηκε στο WiFi!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Ενεργοποίηση ThingSpeak
+    ThingSpeak.begin(client);
+}
+
+void loop() {
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+    
+    long duration = pulseIn(ECHO_PIN, HIGH);
+    float distance = duration * 0.034 / 2;  // Υπολογισμός απόστασης σε cm
+    
+    Serial.print("Απόσταση: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+
+    if (distance<4){
+      Serial.println("Ο κάδος είναι γεμάτος!");
+      }
+
+    int state = digitalRead(TILT_PIN);  
+
+    if (state == HIGH) {
+        Serial.println("Κλίση ανιχνεύτηκε! Πιθανή ανατροπή κάδου...");
+    } else {
+        Serial.println("Χωρίς κλίση.");
+    }
+
+    float t = dht.readTemperature();
+    Serial.print("Θερμοκρασία: ");
+    Serial.print(t);
+    Serial.print(" *C \n");
+    
+    if (t>70){
+      Serial.print("Προσοχή!! κίνδυνος πυρκαγιάς...");
+      }
+
+      // Αποστολή στο ThingSpeak
+    ThingSpeak.setField(1, distance);   // πληρότητα κάδου
+    ThingSpeak.setField(2, state);   // κλίση
+    ThingSpeak.setField(3, t);  // θερμοκρασία
+    
+    int x = ThingSpeak.writeFields(channelID, writeAPIKey);
+    if (x == 200) {
+        Serial.println("Επιτυχής αποστολή στο ThingSpeak.");
+    } else {
+        Serial.print("Σφάλμα αποστολής. Κωδικός: ");
+        Serial.println(x);
+    }
+   
+    delay(20000);  // Αναμονή 20 δευτερόλεπτα  
+
+}
